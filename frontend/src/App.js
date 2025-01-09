098426c9defee1364c72fb7da9c099a286b39234
SJ70 <seojae70@gmail.com>
2025-01-09 23:57:27 +0900

    feat: 기초 ui 디자인 작업
    - header
      - pc 및 태블릿 버전에서 nav 버튼 제공
    - footer
      - 모바일 버전에서 nav 버튼 제공

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,52 @@
+import './index.css';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
+      
+      <header>
+        <div className='nav-bar'>
+          <p className='title'> 포케띠부덱스 </p>
+          <div className='btns'>
+            <div className='btn'>
+              <span class="material-symbols-outlined"> collections_bookmark </span>
+            </div>
+            <div className='btn'>
+              <span class="material-symbols-outlined"> published_with_changes </span>
+            </div>
+            <div className='btn'>
+              <span class="material-symbols-outlined"> mail </span>
+            </div>
+            <div className='btn'>
+              <span class="material-symbols-outlined"> account_circle </span>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <body>
+      </body>
+
+      <footer>
+          <div className='btn'>
+            <span class="material-symbols-outlined"> collections_bookmark </span>
+            <p> 내 씰 </p>
+          </div>
+          <div className='btn'>
+            <span class="material-symbols-outlined"> published_with_changes </span>
+            <p> 교환 </p>
+          </div>
+          <div className='btn'>
+            <span class="material-symbols-outlined"> mail </span>
+            <p> 메시지 </p>
+          </div>
+          <div className='btn'>
+            <span class="material-symbols-outlined"> account_circle </span>
+            <p> 내 정보 </p>
+          </div>
+      </footer>
+
     </div>
   );
 }
