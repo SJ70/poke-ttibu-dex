35356c3eea3cb22f9aa04cb037fa2da7fcf94aa2
SJ70 <seojae70@gmail.com>
2025-01-10 20:28:29 +0900

    feat: 씰 데이터 추가
    - 씰 시리즈 정보
      - 시리즈명
      - 출시일
    
    - 씰 정보
      - 전체 인덱스
      - 시리즈 인덱스
      - 씰 code
        - 시리즈 인덱스와 시리즈 별 인덱스를 참조하여 생성
      - 포켓몬 이름
      - 포켓몬 도감 번호
    
    - 씰 이미지
      - 파일명은 씰 code를 따라 감

---
+++ frontend/src/const/seriesInfo.js
@@ -0,0 +1,13 @@
+export const SERIES_INFO = [
+    { release: "2022. 02. 23", title: "2022 1세대", },
+    { release: "2022. 07. 07", title: "2022 2세대", },
+    { release: "2022. 09. 29", title: "2022 할로윈", },
+    { release: "2022. 12. 06", title: "2022 윈터", },
+    { release: "2023. 01. 26", title: "2023 러블리", },
+    { release: "2023. 05. 12", title: "2023 NEW", },
+    { release: "2023. 10. 30", title: "2023 NEW 시즌1", },
+    { release: "2024. 04. 19", title: "2024 NEW 시즌2", },
+    { release: "2024. 05. 03", title: "2024 메가진화", },
+    { release: "2024. 07. 25", title: "2024 픽셀아트", },
+    { release: "2024. 11. 27", title: "2024 NEW 시즌3", }
+]
