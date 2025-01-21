de89ae05377ee1347405ba1f9cd626f797cedb15
SJ70 <seojae70@gmail.com>
2025-01-22 04:10:01 +0900

    chore: 들여쓰기 간격 획일화

--- src/const/seriesInfo.js
+++ src/const/seriesInfo.js
@@ -1,13 +1,13 @@
 export const SERIES_INFO = [
-    { title: "2022 1세대",     release: "2022. 02. 23", count: 159},
-    { title: "2022 2세대",     release: "2022. 07. 07", count: 116},
-    { title: "2022 할로윈",    release: "2022. 09. 29", count: 27},
-    { title: "2022 윈터",      release: "2022. 12. 06", count: 30},
-    { title: "2023 러블리",    release: "2023. 01. 26", count: 30},
-    { title: "2023 NEW",       release: "2023. 05. 12", count: 55},
-    { title: "2023 NEW 시즌1", release: "2023. 10. 30", count: 139},
-    { title: "2024 NEW 시즌2", release: "2024. 04. 19", count: 163},
-    { title: "2024 메가진화",  release: "2024. 05. 03", count: 53},
-    { title: "2024 픽셀아트",  release: "2024. 07. 25", count: 151},
-    { title: "2024 NEW 시즌3", release: "2024. 11. 27", count: 154}
+  { title: "2022 1세대",     release: "2022. 02. 23", count: 159},
+  { title: "2022 2세대",     release: "2022. 07. 07", count: 116},
+  { title: "2022 할로윈",    release: "2022. 09. 29", count: 27},
+  { title: "2022 윈터",      release: "2022. 12. 06", count: 30},
+  { title: "2023 러블리",    release: "2023. 01. 26", count: 30},
+  { title: "2023 NEW",       release: "2023. 05. 12", count: 55},
+  { title: "2023 NEW 시즌1", release: "2023. 10. 30", count: 139},
+  { title: "2024 NEW 시즌2", release: "2024. 04. 19", count: 163},
+  { title: "2024 메가진화",  release: "2024. 05. 03", count: 53},
+  { title: "2024 픽셀아트",  release: "2024. 07. 25", count: 151},
+  { title: "2024 NEW 시즌3", release: "2024. 11. 27", count: 154}
 ]
